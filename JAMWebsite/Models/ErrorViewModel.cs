8eb7ad2d58b1460515de08c901b226c85895a7a6
broth <broth@DESKTOP-O237NHU>
2023-01-16 20:24:56 -0500

    Add project files.

---
+++ JAMWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JAMWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
